58a947b3ef3b278d0035e4da626fe6184e827029
LeCarnotzetVeveysan <ghislain.demael@gmail.com>
2021-03-12 08:45:50 +0100

    j

---
+++ book1_sharktooth/scenes.py
@@ -0,0 +1,3 @@
+from fonctions import *
+
+scene_00 = Scene(00, "Main Street", "The Main Street of the island")
---
+++ book1_sharktooth/text.py
@@ -0,0 +1,12 @@
+
+MainTextScene0 = "Welcome on Shark Tooth Island ! \n in this book, you will have to save Tom,  \
+                    stranded on the small island you sea far in the sea. \n Unfortunately Big Croc \
+                    is in the area so you cannot cross without dying."
+
+MainTextScene1 = "You have entered the old ruins of Shark Tooth Island. \n On the walls there are many \
+                    drawings of Big Croc, fierce, attacking ships and people. \n \
+                    On the last stones you see a kind of message. A bone, a cauldron and a coconut \
+                    followed by a dead shark. \n What may it be ? \n On the other side of the room, \
+                    you see two tunnels, which one will you take ? "
+
+
