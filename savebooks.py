72e2c38eb793e9af5b9ce3e43fae8a9fec646b22
Hippolyte Bach <hippolyte@bach.fr>
2021-03-12 22:46:42 +0100

    Write Page Mis en Place

--- savebooks.py
+++ savebooks.py
@@ -1,5 +1,6 @@
 import json
 from ClassBook import Book, Page
+import os
 #Ceci simule la création d'un livre test avec 2 pages:
 
 
@@ -19,11 +20,22 @@ def FormatPage(thePage):
 
 
 
+TheBook = "TestBook"
+TestPage = {"ID": 1, "title": "Ceci_est_une_page", "Description": "Ceci est une très très longue descripion", "Book":"Le livre test", "choices":{0:{ "Name": "Quit", "Page": 0, "GiveItem": None, "TakeItem": None}, 1:{ "Name": "Take the Sword", "Page": 1, "GiveItem": "Sword", "TakeItem": None}} }
 
-TestPage = {"ID": 1, "title": "Ceci est une page", "Description": "Ceci est une très très longue descripion", "Book":"Le livre test", "choices":{0:{ "Name": "Quit", "Page": 0, "GiveItem": None, "TakeItem": None}, 1:{ "Name": "Take the Sword", "Page": 1, "GiveItem": "Sword", "TakeItem": None}} }
-def WritePage(thePage):
-    with open('./Test.json', 'w', encoding='utf-8') as f:
+def WritePage(theBook, thePage):
+    path = "./books/" + theBook + "/" + str(thePage["ID"]) + "." + thePage["title"] + ".json"
+    if not os.path.exists("./books/" + theBook):
+        os.makedirs("./books/" + theBook)
+    with open(path, 'w', encoding='utf-8') as f:    
         json.dump(thePage, f, indent=4)
+    print("done")
 
 
+WritePage(TheBook, TestPage)
 
+def LoadBook(bookname):
+    if not os.path.exists("./books/" + bookname):
+        return 404
+    else:
+        
