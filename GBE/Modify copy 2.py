44eafd742b16a778ab6e324d0782401e6b558075
Hadouin-Leroy <hleroy@juniorisep.com>
2021-04-10 18:15:53 +0200

    new names version

--- GBE/Modify copy 2.py
+++ GBE/Modify copy 2.py
@@ -6,25 +6,24 @@ def pageFileName(pageData):
     page = 1
     pageID = pageData.get("ID")
     pageTitle = pageData.get("title")
-    pageName = str(pageID) + "." + str(pageTitle) + ".json"
+    pageName = Save.formatString(str(pageTitle)) + ".json"
     return pageName
 
 def deletePage(bookName, page):
     Book = Load.loadBook(bookName)
-    print(Book)
-    if type(page) == type(1):
-        for i in Book:
-            if i['ID'] == page:
-                page = pageFileName(i)
-                ID = i['ID']
-                break
-            
-    os.remove(os.path.join(Load.get_project_root(), 'books', bookName, page ))
-    Book = Load.loadBook(bookName)
     for i in Book:
-        if i['ID'] > int(ID):
-            i['ID'] = i['ID'] - 1
-        print('\n')
-        print(i)
+        if i['ID'] == page:
+            page = pageFileName(i)
+            ID = i['ID']
+            break
+    try: 
+        os.remove(os.path.join(Load.get_project_root(), 'Books', bookName, page ))
+        Book = Load.loadBook(bookName)
+        for i in Book:
+            if i['ID'] > int(ID):
+                i['ID'] = i['ID'] - 1
+            Save.savePage(i)
+    except:
+        print('No page to delete')
 
 deletePage('TheBook', 1)
