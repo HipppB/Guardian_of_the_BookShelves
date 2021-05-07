3f5d41ef556fd04b2b788269b310d1ae89493b3e
chloec0des <chlotoled@gmail.com>
2021-05-07 10:33:01 +0200

    traduction

--- GBE/Menus.py
+++ GBE/Menus.py
@@ -160,7 +160,7 @@ def menuEditBook(LoadedBook):
         elif NumChoice == 4:
             break
         else:
-            print("Their was a problem !")
+            print("There was a problem !")
             
 # Sous menu Edit Book
 
@@ -203,14 +203,14 @@ def menuListPages(LoadedBook, Mode= "List"):
 
     elif Mode == "Delete":
         Ba.printTitle("Select a page to delete" + LoadedBook , True)
-        Ba.printTitle("/!\\ Cannot be delete ! /!\\" + LoadedBook , True)
+        Ba.printTitle("/!\\ Cannot be deleted ! /!\\" + LoadedBook , True)
         Ba.emptyLine()
         Ba.line()
         for Page in ListPage[1]:
             Ba.printSentence("- " + Page)   
         NumChoice = Ba.inputText(textBefore="Enter the name of the page you want to delete : ", FromList= ListPage[1])
         #Call the delete function with parameter "page" to delete a page
-        input("TO DO mais ca à bien recu l'ordre de supprimer la page suivante : " + NumChoice)
+       
         Modify.deletePage(LoadedBook, ListPage[1].index(NumChoice))
 
 
