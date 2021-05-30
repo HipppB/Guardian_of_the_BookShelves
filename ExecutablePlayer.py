e2a32b491bade0492c7e681f1cdc9e58b063435f
Hippolyte Bach <hippolyte@bach.fr>
2021-05-30 12:25:51 +0200

    Base Player (Lecture de livre possible)

--- ExecutablePlayer.py
+++ ExecutablePlayer.py
@@ -1,10 +1,3 @@
 from GBE import Player, Load,Basics as Ba
 
 Player.mainMenu()
-
-Page = Load.loadPage("The gardians of the Bookshelves' Adventure", "0.Book_Description.json")
-
-Player.PageMenu(Page)
-
-Ba.printSentence("End of book")
-Ba.line()
--- GBE/Player.py
+++ GBE/Player.py
@@ -1,54 +1,35 @@
 from GBE import Basics as Ba
 from GBE import Load
 from GBE import Menus as Me
+import time
+
 
 def mainMenu():
-    print(Ba.playerASCII)
-    Me.menuListBook(EditMode="Return")
-
-def BookMenu(LoadedBook):
+    inventory = {}
     Ba.clear()
-    Ba.line()
-    Ba.printTitle(LoadedBook["title"])
-    pass
-    while True:
-        Ba.clear()
-        Ba.line()
-        Ba.printTitle("Please select a book to read", centered=True)
-        Ba.emptyLine()
-        Ba.printSentence("List of the books you can play :")
-        ListBooks = Load.listBook()
-        Me.ListTheBooks(ListBooks, Mode = "ID")
-        Ba.emptyLine()
-        Ba.line()
-        listchoices = ["Play a Book", "quit"]
-        NumChoice = Ba.Choice(listchoices)
-        Ba.printSentence("You choose the choice number " + str(NumChoice))
-        Ba.line()
-        if NumChoice == 0:
-            LoadedBook = {}
-        if NumChoice == 1:
-            break
-
-    Ba.printTitle("Please select a book to read", centered=True)
-    Ba.emptyLine()
-    Ba.printSentence("List of the books you can play :")
-    ListBooks = Load.listBook()
-    Me.ListTheBooks(ListBooks, Mode = "ID")
+    print(Ba.playerASCII)
+    print("Loading...")
+    time.sleep(1)
+    LoadedBook = Me.menuListBook(EditMode="Return")
+    ListPages = Load.listPages(LoadedBook)
+    LoadedPage = Load.loadPage(LoadedBook, ListPages[0][0])
+    print(LoadedPage)
+    PageMenu(LoadedPage)
+    while Ba.inputText("Do you want to restart the book ?", FromList=["Yes", "yes", "no", "No"]).lower() == "yes":
+        PageMenu(LoadedPage)
 
 def PageMenu(Page):
     Ba.clear()
     Ba.line()
+    Ba.printTitle(Page['Book'], centered=True)
     Ba.printTitle(Page['title'], centered=True)
     Ba.emptyLine()
     Ba.printSentence((Page['Description']))
     Ba.emptyLine()
     Ba.line()
-
     if len(Page['choices']) == 0:
         return False
     else:
-
         listChoicePage = [Page['choices'][str(i)]['Name'] for i in Page['choices']]
         choice = Ba.Choice(listChoicePage)
         Ba.line()
